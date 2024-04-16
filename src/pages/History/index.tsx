5be70cb607b88d7c1df9bb5c29a64e9654cd8ead
o-Franco <118946545+o-Franco@users.noreply.github.com>
2024-04-15 23:44:06 -0300

    Paginas e Rotas finalizados

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
   
@@ -21,25 +21,33 @@ export function History() {
               <td>Tarefas</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor = "green">Concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefas</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor = "green">Concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefas</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor =  "red">Interrompido</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefas</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor = "yellow">Em andamento</Status>
+              </td>
             </tr>
 
           </tbody>
