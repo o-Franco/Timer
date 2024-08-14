ef6c322cee65cd671ebc11ac2081acb1111f6f5e
o-Franco <118946545+o-Franco@users.noreply.github.com>
2024-08-14 00:11:38 -0300

    Armazenando Ciclo ativo para manter na tela apos refresh v1.0.2

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -2,7 +2,7 @@ import { ReactNode, createContext, useEffect, useReducer, useState } from "react
 import { Cycle, cyclesReducer } from '../reducers/cycles/reducer'
 import { act } from "react-dom/test-utils";
 import { ActionTypes, addNewCycleAction, interruptCurrentCycleAction, markCurrentCycleAsFinishedAction } from "../reducers/cycles/actions";
-import { parse } from "date-fns";
+import { differenceInSeconds, parse } from "date-fns";
 
 interface CreateCycleData {
     task: string;
@@ -34,15 +34,24 @@ export function CyclesContextProvider({
     {
       cycles:[],
       activeCycleId: null,
-    }, () => {
+    }, (initialState) => {
       const storedStateAsJSON = localStorage.getItem('@ignite-timer:cycles-state-1.0.0');
 
       if (storedStateAsJSON){
         return JSON.parse(storedStateAsJSON)
       }
+      return initialState
     })
 
-    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+    const {cycles, activeCycleId} = cyclesState;
+    const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+    const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+      if(activeCycle){
+        return differenceInSeconds(new Date(), activeCycle.startDate)
+      }
+      return 0
+    })
 
     useEffect(() => {
       const stateJSON = JSON.stringify(cyclesState)
@@ -50,8 +59,6 @@ export function CyclesContextProvider({
       localStorage.setItem('@ignite-timer:cycles-state-1.0.0', stateJSON)
     },[cyclesState])
 
-    const {cycles, activeCycleId} = cyclesState;
-    const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
     function setSecondsPassed(seconds: number){
         setAmountSecondsPassed(seconds)
